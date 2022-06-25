7c8d708998c4503834a81738eb3796f06f203a9f
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2022-06-26 02:27:19 +0500

    implement number check condition

--- number_guessing.py
+++ number_guessing.py
@@ -2,6 +2,7 @@ import random
 from tkinter import *
 
 random_number = random.randint(0, 999)
+print(random_number)
 
 root = Tk()
 root.geometry('500x500')
@@ -24,40 +25,37 @@ def printAttempts(_attempts):
     label_attempt.place(x=90, y=100)
 
 
-def printSuccess():
-    success_message = Label(root,
-                            text="Horray you guessed it right",
-                            width=20,
-                            font=("bold", 10))
-    success_message.place(x=80, y=200)
-
-
-def printFailure():
-    failure_message = Label(root,
-                            text="You gussed wrong number :(",
-                            width=20,
-                            font=("bold", 10))
-    failure_message.place(x=80, y=200)
-
-
 printAttempts(3)
 
 
-def onSubmit():
-    attempts = 3
+def printMessage(msg):
+    _message = Label(root, text=msg, width=50, font=("bold", 10))
+    _message.place(x=80, y=200)
 
+
+def onSubmit():
+
+    attempts = 3
     guessed_number = entry_1.get()
     guessed_number = int(guessed_number)
 
     while attempts != 0:
         if (guessed_number == random_number):
-            printSuccess()
+            printMessage("Horray you just got it Right!")
+            attempts = 0
+            break
         else:
-            printFailure()
             attempts -= 1
+            printMessage("You got it wrong :(")
+            if (guessed_number > random_number):
+                printMessage("Your guess number is greater than lucky number")
+            else:
+                printMessage("Your guess number is less than lucky number")
             printAttempts(attempts)
+            continue
             if (attempts == 0):
-                print("No attempts left, try again later")
+                printMessage("No attempts left, try again later")
+                break
 
 
 Button(root, text='Submit', width=20, bg='brown', fg='white',
